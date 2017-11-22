7dcbe69893c562dc0f804f7662b22d294aa186b6
Victoria Michalowski <victoria.michalowski@gmail.com>
2017-11-21 21:25:56 -0800

    final commits

--- hw08 - building shiny apps/ui and server folder/server.R
+++ hw08 - building shiny apps/ui and server folder/server.R
@@ -1,7 +1,9 @@
 server <- function(input, output) {
   
   download.file("https://github.com/vmichalowski/STAT545-hw-Michalowski-Victoria/raw/master/hw08%20-%20building%20shiny%20apps/baby_namesLong.csv", destfile="baby_namesLong.csv")
-  baby_namesLong <- read_csv("baby_namesLong.csv")
+  baby_namesLong <- read.csv("baby_namesLong.csv")
+  library(shiny)
+  library(tidyverse)
   
   filtered <- reactive({
     if (is.null(input$NameInput)) {
@@ -14,22 +16,31 @@ server <- function(input, output) {
              Name == input$NameInput)
   })
 
-  output$plot <- renderPlot({
+  output$PlotOutput <- renderPlot({
     if (is.null(filtered())) {
       return()
     }
     
     ggplot(filtered(), aes(x=Year, y=Frequency)) +
-      geom_line()
+      geom_line() +
+      labs(x = "Year", y = "Frequency", caption = "Data Source: Ministry of Health - British Columbia Vital Statistics Agency") +
+      theme_minimal()
   })
   
-  output$results <- renderTable({
+  output$TableOutput <- DT::renderDataTable({
     filtered()
   })
   
   output$NameOutput <- renderUI({
-    selectInput("NameInput", "Name",
-                sort(unique(baby_namesLong$Name)),
-                selectize=FALSE)
+    if (input$SexInput == 1)
+      return(selectInput("NameInput", "Name",
+                         sort(unique(subset.data.frame(baby_namesLong, Sex == 1, select = Name, drop = TRUE))),
+                         selected = "AARON",
+                         selectize=FALSE))
+    if (input$SexInput == 2)
+      return(selectInput("NameInput", "Name",
+                         sort(unique(subset.data.frame(baby_namesLong, Sex == 2, select = Name, drop = TRUE))),
+                         selected = "AUDREY",
+                         selectize = FALSE))
   })
 }
--- hw08 - building shiny apps/ui and server folder/ui.R
+++ hw08 - building shiny apps/ui and server folder/ui.R
@@ -5,18 +5,18 @@ ui <- fluidPage(
     sidebarLayout(#inputs go here
        
        sidebarPanel(
-          sliderInput("YearInput", "Year", min=1915, max=2014,
-                  value = c(2000, 2014), sep = ""),
-          radioButtons("SexInput", "Sex", choiceValues = c("1", "2"), choiceNames=c("Boy", "Girl")),
-          uiOutput("NameOutput")
+         sliderInput("YearInput", "Year", min=1915, max=2014,
+                     value = c(1935, 2014), sep = ""),
+         radioButtons("SexInput", "Sex", choiceValues = c("1", "2"), choiceNames = c("Boy", "Girl"), selected = "2"),
+         uiOutput("NameOutput")
           ),
+        
           
         mainPanel(#results go here
-          plotOutput("plot"),
-          br(),
-          br(),
-          tableOutput("results")
-                  )
+          tabsetPanel(
+            tabPanel("Graph", plotOutput("PlotOutput")),
+            tabPanel("Table", DT::dataTableOutput("TableOutput")))
+          )
     )
 
 )
